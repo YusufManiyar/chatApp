819a6ea4516050b07e0d0cb63e4582e4ba906aa6
Tejas150 <tejaskamble1629@gmail.com>
2024-07-06 01:17:23 +0530

    Add frontend app url for cors

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({extended: false}))
 // Middleware
 app.use(bodyParser.json());
 app.use(cors({
-    origin: 'http://127.0.0.1:5500'
+    origin: process.env.CHATMET_FRONTEND_URL
 }));
 
 // Routes
