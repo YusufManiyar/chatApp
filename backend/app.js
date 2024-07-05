952ae7bf3455d5339e79696ee9f5b1cef0eda0d9
Tejas150 <tejaskamble1629@gmail.com>
2024-07-06 01:24:26 +0530

    add cors url

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({extended: false}))
 // Middleware
 app.use(bodyParser.json());
 app.use(cors({
-    origin: process.env.CHATMET_FRONTEND_URL
+    origin: process.env.CHATMET_FRONTEND_URL || 'https://chatmet.netlify.app/'
 }));
 
 // Routes
