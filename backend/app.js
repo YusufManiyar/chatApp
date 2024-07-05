e6746da32785709623aedeacd2daf0bd5b3ba1da
Tejas150 <tejaskamble1629@gmail.com>
2024-07-06 01:27:47 +0530

    debugging

--- backend/app.js
+++ backend/app.js
@@ -14,9 +14,10 @@ app.use(bodyParser.urlencoded({extended: false}))
 // Middleware
 app.use(bodyParser.json());
 app.use(cors({
-    origin: process.env.CHATMET_FRONTEND_URL || 'https://chatmet.netlify.app/'
+    origin: 'https://chatmet.netlify.app/'
 }));
 
+console.log('frontend url=>', process.env.CHATMET_FRONTEND_URL, typeof process.env.CHATMET_FRONTEND_URL)
 // Routes
 app.use('/user', userRoutes);
 app.use('/message', messageRoutes);
