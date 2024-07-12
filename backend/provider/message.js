002ba8aa93ff84d855d69ee7431cae68ef903c30
Tejas150 <tejaskamble1629@gmail.com>
2024-07-12 14:39:23 +0530

    complete file sharing system to my chat application

--- backend/provider/message.js
+++ backend/provider/message.js
@@ -4,20 +4,30 @@ const { uploadFile } = require('./fileSystem.js')
 
 module.exports = {
     addMessage: async (wss, ws, body) => {
-        const { receiverId, message, attachment } = JSON.parse(body);
+        console.log('body==>\n\n',  body, '\n\n')
+        const buffer = Buffer.from(body);
+
+        // Parse the JSON part first
+        const jsonPartEndIndex = buffer.indexOf('}') + 1;
+        const jsonPart = buffer.slice(0, jsonPartEndIndex).toString();
+        const data = JSON.parse(jsonPart);
+        const fileBuffer = buffer.slice(jsonPartEndIndex);
+    
         const user = ws.user;
         const senderId = user.id;
         try {
             let fileUrl
-            if(attachment) {
+            if(data.hasFile) {
                 //write code to upload file to cloud and get the url
-                fileUrl = await uploadFile(attachment)
+                console.log(fileBuffer,'c=>c attachment')
+                fileUrl = await uploadFile(fileBuffer)
             }
-
-            const newMessage = await Message.create({ senderId, receiverId, message, attachment: fileUrl });
+            delete data.hasFile
+            console.log('data=>\n', { senderId, ...data, attachment: fileUrl })
+            const newMessage = await Message.create({ senderId, ...data, attachment: fileUrl });
 
             wss.clients.forEach((client) => {
-                if (client.readyState === WebSocket.OPEN && client.user.groups.includes(receiverId)) {
+                if (client.readyState === WebSocket.OPEN && client.user.groups.includes(data.receiverId)) {
                     client.send(JSON.stringify(newMessage));
                 }
             });
